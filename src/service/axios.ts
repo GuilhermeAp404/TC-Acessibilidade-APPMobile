4382cfa950b7c1e76c6cb9b90f23fffecf541a5d
Guilherme Faria <guilherme.ap404@gmail.com>
2024-09-11 20:20:13 -0300

    Ajuste

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"https://tc-acessibilidade-api-production.up.railway.app"
+    baseURL:process.env.EXPO_PUBLIC_API_URL
 })
 
 export default api;
